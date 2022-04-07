8c78fa8bb8c9aa6a50c0326bb8ec98ab8e79cfd9
sksat <sksat@sksat.net>
2022-04-07 21:47:59 +0900

    add wasm tani

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,20 @@
+extern crate wasm_bindgen;
+use anyhow::Context;
+use wasm_bindgen::prelude::*;
 
+pub mod config;
+pub mod pattern;
+
+use config::Config;
+
+#[wasm_bindgen]
+pub fn tani(config: &str) {
+  let config: Config = toml::from_str(config)
+    .with_context(|| "toml parser error")
+    .unwrap();
+
+  let v = pattern::coins::check(&config).unwrap();
+  for (name, msg) in v.iter() {
+    println!("{}: {}", name, msg)
+  }
+}
