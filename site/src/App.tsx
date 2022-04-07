9639792307a6e98c711a59fed6f0bf4a1d0b9c48
sksat <sksat@sksat.net>
2022-04-07 23:09:50 +0900

    just works

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -8,7 +8,7 @@ export const App: React.FC = () => {
   return (
     <>
       <H1>Hello</H1>
-      <Button onClick={() => tani("hoge")}>tani</Button>
+      <Button onClick={() => tani("[[class]]\n  id = \"31HG122\"\n  name = \"English Reading Skills I\"\n  credits = 1.0")}>tani</Button>
     </>
   );
 };
--- src/lib.rs
+++ src/lib.rs
@@ -8,13 +8,20 @@ pub mod pattern;
 use config::Config;
 
 #[wasm_bindgen]
+extern "C" {
+  fn alert(s: &str);
+}
+
+#[wasm_bindgen]
 pub fn tani(config: &str) {
+  alert(&config);
   let config: Config = toml::from_str(config)
     .with_context(|| "toml parser error")
     .unwrap();
 
   let v = pattern::coins::check(&config).unwrap();
   for (name, msg) in v.iter() {
-    println!("{}: {}", name, msg)
+    let s = format!("{}: {}", name, msg);
+    alert(&s);
   }
 }
